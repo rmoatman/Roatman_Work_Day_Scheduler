6ed1e09203cf6375b03bd9c6f2afaefdf3972583
RMOATMAN <raemarie.oatman@gmail.com>
2021-05-25 14:44:47 -0400

    Changed div container from table to individual divs

--- Assets/script.js
+++ Assets/script.js
@@ -3,7 +3,10 @@
 
         // VARIABLES //
         timerEl = document.querySelector("#currentDay"); // creates a variable for the current date and time displayed in the header //
+        event9El = document.querySelector("#event9"); // creates a variable for the content of the 9am form //
+        del9amEl = document.querySelector("#del9am"); // creates a variable for the button to delete 9am text //
         var now; // initializes variable used for current time and date //
+
 
 
         // DATE AND TIME //
@@ -17,7 +20,26 @@
             setInterval(now, 1000);
         // end of Date and Time //
 
+
+
+
+        // starter script for delete buttons //
+        function blah() {
+            alert("clicked");
+
+        }
+
+        del9amEl.addEventListener("click", blah);
+
+        // end of starter script for delete buttons //
+
+
+
+
+
+       
 /* allow user to add/edit events directly in the cell */
+/* store events in an object */
 /* add an event listener for the delete buttons */
 /* change table colors depending upon the time of day */
 /* store data time:event to local storage */
