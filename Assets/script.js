e226318d5c94f26efdde981cd6b9086bf6de074f
RMOATMAN <raemarie.oatman@gmail.com>
2021-05-29 00:44:19 -0400

    updated save button code and readme.md

--- Assets/script.js
+++ Assets/script.js
@@ -1,11 +1,13 @@
 // LIST OF FUNCTIONS //
+    /* Date and Time - This (now = function() places the date and time in the header and updates it every second */
+    /* loadPage() - Loads local data to stored array and copies to screen when page is loaded or refreshes */
+    /* comparetest() - When the page reloads or is refreshed, checks for time of day and colors row appropriately */
+    /* checkforLocalData() - This function prevents errors if the page loads with no localStorage */
 
 
 // VARIABLES //
-    timerEl = document.querySelector("#currentDay"); // creates a variable for the current date and time displayed in the header //
-    var now; // initializes variable used for current time and date //
-    var rightnow = parseInt(moment().format("HH")); // creates a variable that returns the hour in military time //
-    var localcalendar = [];
+    timerEl = document.querySelector("#currentDay"); /* creates a variable for the current date and time displayed in the header */
+    var now; /* initializes variable used for current time and date */
 
     /* These variables are used in updateCalendarArray() and loadPage() to assist in updating the additions and subtractions to each hourly events listed int he textareas */
     var el09e = document.querySelector("#event09");
@@ -35,7 +37,7 @@
         {
             time: "",
             event: "",
-            mtime: 12
+            mtime: 11
         },
         
         {
@@ -77,16 +79,7 @@
     ];
 
 
-/* FUNCTIONS */
-    // Date and time //
-        // This code places the date and time in the header and updates it every second //
-        (now = function() {
-            timerEl.innerHTML = moment().format('MMMM Do YYYY, h:mm:ss a');
-        })();
-
-        setInterval(now, 1000);
-    // end of Date and Time //
-
+// FUNCTIONS //
     /* Load Local Data Storage */
         /* Loads local data to stored array and copies to screen when page is loaded or refreshes */
         function loadPage(){
@@ -99,144 +92,147 @@
                 el15e.textContent = calendar[6].event;
                 el16e.textContent = calendar[7].event;
                 el17e.textContent = calendar[8].event;
-            
-        }
+       }
     /* end of Load Local Data Storage */
 
     /* Prioritize Rows by Color */
         /* When the page reloads or is refreshed, checks for time of day and colors row appropriately */
         function comparetest() {
             console.log("comparetest()");
-            console.log("rightnow: " + rightnow);
-            console.log(calendar[0].mtime + "hour");
+            var rightnow = parseInt(moment().format("HH")); // creates a variable that returns the hour in military time //
+   
             if (calendar[0].mtime > rightnow){
-                rnine.classList.add("past");
+                $(rnine).removeClass();
+                $(rnine).addClass("row future");
+
             } else {
                 if (calendar[0].mtime == rightnow){
-                    rnine.classList.add("present");
+                    $(rnine).removeClass();
+                    $(rnine).addClass("row present");
+                    
                 } else {
-                    rnine.classList.add("future");
+                    $(rnine).removeClass();
+                    $(rnine).addClass("row past");
                 }
             }
 
-            console.log("rightnow: " + rightnow);
-            console.log(calendar[1].mtime + "hour");
             if (calendar[1].mtime > rightnow){
-                rten.classList.add("past");
+                $(rten).removeClass();
+                $(rten).addClass("row future");
+
             } else {
                 if (calendar[1].mtime == rightnow){
-                    rten.classList.add("present");
+                    $(rten).removeClass();
+                    $(rten).addClass("row present");
                 } else {
-                    rten.classList.add("future");
+                    $(rten).removeClass();
+                    $(rten).addClass("row past");
                 }
             }
 
-            console.log("rightnow: " + rightnow);
-            console.log(calendar[2].mtime + "hour");
             if (calendar[2].mtime > rightnow){
-                releven.classList.add("past");
+                $(releven).removeClass();
+                $(releven).addClass("row future");
+
             } else {
                 if (calendar[2].mtime == rightnow){
-                    releven.classList.add("present");
+                    $(releven).removeClass();
+                    $(releven).addClass("row present");
+
                 } else {
-                    releven.classList.add("future");
+                    $(releven).removeClass();
+                    $(releven).addClass("row past");
                 }
             }
 
-            console.log("rightnow: " + rightnow);
-            console.log(calendar[3].mtime + "hour");
             if (calendar[3].mtime > rightnow){
-                rtwelve.classList.add("past");
+                $(rtwelve).removeClass();
+                $(rtwelve).addClass("row future");
+
             } else {
                 if (calendar[3].mtime == rightnow){
-                    rtwelve.classList.add("present");
+                    $(rtwelve).removeClass();
+                    $(rtwelve).addClass("row present");
+                    
                 } else {
-                    rtwelve.classList.add("future");
+                    $(rtwelve).removeClass();
+                    $(rtwelve).addClass("row past");
                 }
             }
 
-            console.log("rightnow: " + rightnow);
-            console.log(calendar[4].mtime + "hour");
             if (calendar[4].mtime > rightnow){
-                rone.classList.add("past");
+                $(rone).removeClass();
+                $(rone).addClass("row future");
             } else {
                 if (calendar[4].mtime == rightnow){
-                    rone.classList.add("present");
+                    $(rone).removeClass();
+                    $(rone).addClass("row present");
                 } else {
-                rone.classList.add("future");
+                    $(rone).removeClass();
+                    $(rone).addClass("row past");
                 }
             }
 
-            console.log("rightnow: " + rightnow);
-            console.log(calendar[5].mtime + "hour");
             if (calendar[5].mtime > rightnow){
-                rtwo.classList.add("past");
+                $(rtwo).removeClass();
+                $(rtwo).addClass("row future");
             } else {
                 if (calendar[5].mtime == rightnow){
-                    rtwo.classList.add("present");
+                    $(rtwo).removeClass();
+                    $(rtwo).addClass("row present");
                 } else {
-                rtwo.classList.add("future");
+                    $(rtwo).removeClass();
+                    $(rtwo).addClass("row past");
                 }
-            } 
+            }
 
-            console.log("rightnow: " + rightnow);
-            console.log(calendar[6].mtime + "hour");
             if (calendar[6].mtime > rightnow){
-                rthree.classList.add("past");
+                $(rthree).removeClass();
+                $(rthree).addClass("row future");
+
             } else {
                 if (calendar[6].mtime == rightnow){
-                    rthree.classList.add("present");
+                    $(rthree).removeClass();
+                    $(rthree).addClass("row present");
+
                 } else {
-                rthree.classList.add("future");
+                    $(rthree).removeClass();
+                    $(rthree).addClass("row past");
                 }
             }
 
-            console.log("rightnow: " + rightnow);
-            console.log(calendar[7].mtime + "hour");
             if (calendar[7].mtime > rightnow){
-                rfour.classList.add("past");
+                $(rfour).removeClass();
+                $(rfour).addClass("row future");
+
             } else {
                 if (calendar[7].mtime == rightnow){
-                    rfour.classList.add("present");
+                    $(rfour).removeClass();
+                    $(rfour).addClass("row past");
+
                 } else {
-                rfour.classList.add("future");
+                    $(rfour).removeClass();
+                    $(rfour).addClass("row past");
                 }
             }
 
-            console.log("rightnow: " + rightnow);
-            console.log(calendar[8].mtime + "hour");
             if (calendar[8].mtime > rightnow){
-                rfive.classList.add("past");
+                $(rfive).removeClass();
+                $(rfive).addClass("row future");
+
             } else {
                 if (calendar[8].mtime == rightnow){
-                    rfive.classList.add("present");
+                    $(rfive).removeClass();
+                    $(rfive).addClass("row present");
+
                 } else {
-                rfive.classList.add("future");
+                    $(rfive).removeClass();
+                    $(rfive).addClass("row past");
                 }
             }
-        };
-    /* end of Prioritize Rows by Color */
+        }
+    /* end of comparetest() */
 
-
-    /* Update Local Storage on Save */
-        /* When a save button is selected, the calendar array is updated and then saved to local storage.  Executed when a save button is pressed */
-        function updateCalendarArray() {
-            console.log("updateCalendarArray");
-
-            calendar[0].event = el09e.value;
-            calendar[1].event = el10e.value;
-            calendar[2].event = el11e.value;
-            calendar[3].event = el12e.value;
-            calendar[4].event = el13e.value;
-            calendar[5].event = el14e.value;
-            calendar[6].event = el15e.value;
-            calendar[7].event = el16e.value;
-            calendar[8].event = el17e.value;
-
-            /* saving to local storage */
-            localStorage.setItem("calendar", JSON.stringify(calendar));
-        };
-    /* end of updateCalendarArray() */
 
     /* This function prevents errors if the page loads with no localStorage */
         function checkforLocalData() {
@@ -249,11 +245,18 @@
     /* end of checkforLocalData() */
 
 
-
 /* EXECUTION */
 
-    /* This code is executed first in order to update any events stored locally and assign appropriate colors to the hours */
+    /* Date and Time */
+        /* This code places the date and time in the header and updates it every second */
+        (now = function() {
+            timerEl.innerHTML = moment().format('MMMM Do YYYY, h:mm:ss a');
+        })();
 
+        setInterval(now, 1000);
+    /* end of Date and Time */
+
+    /* This code is executed immediately after getting the date and time in order to update any events stored locally and assign appropriate colors to the hours */
     checkforLocalData();
 
     loadPage();
@@ -261,22 +264,54 @@
     /* checks for color changes every minute */
     setInterval(comparetest, 60000);
 
-    /* Listens for any save button to be clicked, then executes the updateCalendarArray function */
-    document.querySelectorAll(".btn").forEach(item => {
-        item.addEventListener('click', updateCalendarArray); {
-        };
+    /* Listen for Save Button and Update Local Storage with the corresponding event  */
+    $("button").click(function(){
+        var btnclicked = $(this).attr('id');
+        switch (btnclicked) {
+            case "9": 
+                calendar[0].event = el09e.value;
+            break;
+
+            case "10":
+                calendar[1].event = el10e.value;
+            break;
+
+            case "11":
+                calendar[2].event = el11e.value;
+            break;
+            
+            case "12":
+                calendar[3].event = el12e.value;
+            break;
+            
+            case "13":
+                calendar[4].event = el13e.value;
+            break;
+
+            case "14":
+                calendar[5].event = el14e.value;
+            break;
+
+            case "15":
+                calendar[6].event = el15e.value;
+            break;
+
+            case "16":
+                calendar[7].event = el16e.value;
+            break;
+
+            case "17":
+                calendar[8].event = el17e.value;
+            break;
+        }
+
+        /* saving to local storage */
+        localStorage.setItem("calendar", JSON.stringify(calendar));
+
     });
-
-      
-
-// To Do //
-
-/* check for responsiveness */
-/* checkpoint time left aligned and icon right aligned.  Headers, too. */
-/* make sure color code removes classes and rename variables */
-/* need to make each button work independently or may overwrite when local saving */
+    /* end of listen for save button and update local storage */
 
 
-// Last Revised 5/27/21 //
+// Last Revised 5/29/21 //
 // Raemarie Oatman raemarie.oatman@gmail.com //
 // Homework Week 5 - 05 Third-Party APIs: Work Day Scheduler //
